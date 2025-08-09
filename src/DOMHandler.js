1384bac4d9ef869754d3fc311d5046918d0e1e56
MalekToumi-815 <191920560+MalekToumi-815@users.noreply.github.com>
2025-08-09 02:24:03 +0100

    changed imported element from domhandler

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -1,15 +1,18 @@
-function newTodo (todo) {
-    const todoList = document.querySelector('.TodoList');
-    const todoItem = document.createElement('div');
-    todoItem.className = 'todo-item';
-    todoItem.innerHTML = `
-        <h3>${todo.title}</h3>
-        <p>${todo.description}</p>
-        <p>Due: ${todo.dueDate}</p>
-        <p>Priority: ${todo.priority}</p>
-        <p>Category: ${todo.category}</p>
-        <button class="delete-todo" data-id="${todo.id}">Delete</button>
-    `;
-    todoList.appendChild(todoItem);
-}
-export { newTodo };
+const DOM = (() => {
+    function newTodo (todo) {
+        const todoList = document.querySelector('.TodoList');
+        const todoItem = document.createElement('div');
+        todoItem.className = 'todo-item';
+        todoItem.innerHTML = `
+            <h3>${todo.title}</h3>
+            <p>${todo.description}</p>
+            <p>Due: ${todo.dueDate}</p>
+            <p>Priority: ${todo.priority}</p>
+            <p>Category: ${todo.category}</p>
+            <button class="delete-todo" data-id="${todo.id}">Delete</button>
+        `;
+        todoList.appendChild(todoItem);
+    }
+    return { newTodo };
+})();
+export { DOM };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {Todos} from './TodosHandler.js';
-import { newTodo } from './DOMHandler.js';
+import { DOM } from './DOMHandler.js';
 // Event listener for the new todo form submission
 const form = document.getElementById('newtodoForm');
 form.addEventListener('submit', function (event) {
@@ -12,7 +12,7 @@ form.addEventListener('submit', function (event) {
     // Logic to handle the new todo item
     console.log(`New Todo: ${title}, ${description}, ${priority}, ${category}, ${dueDate}`);
     const newtodo = Todos.addTodo(title, description, dueDate, priority, category);
-    newTodo(newtodo);
+    DOM.newTodo(newtodo);
     // Reset the form after submission
     form.reset();
 });
